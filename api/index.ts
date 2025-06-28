e39c24643fb30cd1a7f3e19fcc518a7b2296f464
Tattzy25 <tattzy2025@gmail.com>
2025-06-27 18:13:43 -0700

    Your update description

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { registerRoutes } from "../server/routes";
+import { serveStatic, log } from "../server/vite";
 
 const app = express();
 app.use(express.json());
@@ -39,8 +39,9 @@ app.use((req, res, next) => {
   next();
 });
 
+// Initialize the app
 (async () => {
-  const server = await registerRoutes(app);
+  await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
@@ -50,23 +51,9 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
-
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
-    port,
-    host: "localhost",
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  // In production, serve static files
+  serveStatic(app);
 })();
+
+// Export the Express app for Vercel
+export default app;
